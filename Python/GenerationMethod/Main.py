af553b14b798aa1e82cab51f90695d17edd2ca42
LEOLEOl <nguyenhuynhleo@gmail.com>
2017-05-10 15:42:09 +0700

    # Python 10:00

--- Python/GenerationMethod/Main.py
+++ Python/GenerationMethod/Main.py
@@ -1,13 +1,14 @@
 import hashlib as hl
-from Arrangement import *
 from RepeatedArrangement import *
 
-def brute_force(type, size_pass):
+
+def brute_force_attack(type, size_pass):
     x, data = [0], ['0']
     for i in range(1, size_pass + 1): x.append(1)  # init array of x (index array)
 
+    # add data
     if type == 1: # 01..9
-        for i in range(0, 10): data.append(chr(48 + i)) # add data
+        for i in range(0, 10): data.append(chr(48 + i))
     elif type == 2: # ab..z
         for i in range(0, 26): data.append(chr(97 + i))
     elif type == 3: # AB..Z
@@ -28,7 +29,7 @@ def brute_force(type, size_pass):
 
     while x is not None:
         s = ""
-        for i in range(1, size_pass + 1): s = s + data[x[i]]
+        for i in range(1, size_pass + 1): s = s + data[x[i]] # join function
         # c = c + 1
         # printf("%d. ", c)
         # print(hl.sha1(s).hexdigest()) # do something about that
@@ -36,5 +37,26 @@ def brute_force(type, size_pass):
         x = next_repeated_arrangement(x, len(data) - 1, size_pass) # get next index array
 
 
-brute_force(2, 5)
-printf("%s ", "DONE")
+def dictionary_attack(file_name):
+    # with open('list.txt') as f:
+    #     lines = f.readlines() # with \n included
+    with open(file_name) as f:
+        lines = f.read().splitlines() # with \n excluded
+    # or
+    #lines = [line.rstrip('\n') for line in open('list.txt')]
+    return lines
+
+
+def generate(word):
+    list = [word]
+
+    return [word]
+
+
+
+#brute_force_attack(2, 5)
+#printf("%s ", "DONE")
+
+lines = dictionary_attack('list.txt')
+for line in lines:
+    printf("%s ", line)
